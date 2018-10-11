7cdecbb44c018730f9f9def7eb0bf32736ccba4b
noisex <noisex@ya.ru>
2018-10-11 23:59:33 +0300

    Филгер

---
+++ Addons/Fliger.lua
@@ -0,0 +1,222 @@
+
+local function UpdateAura( self, unit)
+	--local auraFilter = { "HARMFUL", "HELPFUL"}
+	local fligerTD, fligerTB, fligerPB, fligerPD = 1, 1, 1, 1
+
+	-- DEBUFFS
+	local index = 1
+	local filter = "HARMFUL"
+	while true do
+		local name, icon, count, _, duration, expirationTime, caster, _, _, spellID = UnitAura( unit, index, filter)
+		if not name then break end
+			
+		if yo.fliger.tDebuffEnable then
+			if unit == self.tDebuff.unit and caster == "player"	and DebuffWhiteList[name] then
+				if not self.tDebuff[fligerTD] then self.tDebuff[fligerTD] = CreateAuraIcon( self.tDebuff, fligerTD, true)end
+				UpdateAuraIcon( self.tDebuff[fligerTD], filter, icon, count, nil, duration, expirationTime, spellID, index)
+				fligerTD = fligerTD + 1						
+			end
+		end
+		
+		if yo.fliger.pDebuffEnable then
+			if unit == self.pDebuff.unit and RaidDebuffList[spellID] then
+				if not self.pDebuff[fligerPD] then self.pDebuff[fligerPD] = CreateAuraIcon( self.pDebuff, fligerPD, true)end
+				UpdateAuraIcon( self.pDebuff[fligerPD], filter, icon, count, nil, duration, expirationTime, spellID, index)
+				fligerPD = fligerPD + 1						
+			end
+		end
+
+		index = index + 1
+	end
+
+	-- BUFFS
+	local index = 1
+	local filter = "HELPFUL"
+	while true do
+		local name, icon, count, _, duration, expirationTime, caster, _, _, spellID = UnitAura( unit, index, filter)
+		if not name then break end
+		
+		if yo.fliger.tDebuffEnable then
+			if unit == self.tDebuff.unit and caster == "player" and BuffWhiteList[name] then
+				if not self.tDebuff[fligerTD] then self.tDebuff[fligerTD] = CreateAuraIcon( self.tDebuff, fligerTD, true)end
+				UpdateAuraIcon( self.tDebuff[fligerTD], filter, icon, count, nil, duration, expirationTime, spellID, index)
+				fligerTD = fligerTD + 1	
+			end
+		end
+			
+		if yo.fliger.pBuffEnable then
+			if unit == self.pBuff.unit and PlayerBuffWhiteList[name] then
+				if not self.pBuff[fligerPB] then self.pBuff[fligerPB] = CreateAuraIcon( self.pBuff, fligerPB, true)end
+				UpdateAuraIcon( self.pBuff[fligerPB], filter, icon, count, nil, duration, expirationTime, spellID, index)
+				fligerPB = fligerPB + 1
+			end
+		end
+
+		index = index + 1
+	end	
+
+	-- CLEARING
+	if unit == "target" then
+		for index = fligerTD,	#self.tDebuff	do self.tDebuff[index]:Hide()   end
+	
+	elseif unit == "player"	then
+		for index = fligerPB,	#self.pBuff		do self.pBuff[index]:Hide()   end
+		for index = fligerPD,	#self.pDebuff	do self.pDebuff[index]:Hide()   end
+	end	
+end
+
+local numTabs, totalspellnum
+yo_spellsCD = {}
+
+local function parsespellbook(spellbook)
+	i = 1
+	while true do
+		skilltype, id = GetSpellBookItemInfo(i, spellbook)
+		--if id == 132469 then
+			--print("dasdasdsadsadsad")
+		--end
+		name = GetSpellBookItemName(i, spellbook)
+		if name and skilltype == "SPELL" and spellbook == BOOKTYPE_SPELL and not IsPassiveSpell(i, spellbook) and id ~= 125439 then
+			yo_spellsCD[id] = true
+		end
+		i = i + 1
+		if i >= totalspellnum then i = 1 break end
+		
+		if (id == 88625 or id == 88625 or id == 88625) and (skilltype == "SPELL" and spellbook == BOOKTYPE_SPELL) then
+		   yo_spellsCD[88625] = true
+		   yo_spellsCD[88684] = true
+		   yo_spellsCD[88685] = true
+		end
+	end
+end
+
+local function UpdateSpels()
+	numTabs = GetNumSpellTabs()
+	totalspellnum = 0
+	for i=1,numTabs do
+		local numSpells = select(4, GetSpellTabInfo(i))
+		totalspellnum = totalspellnum + numSpells
+	end
+	parsespellbook(BOOKTYPE_SPELL)	
+end
+
+
+local function UpdateCD(self)
+	local now = GetTime()
+	local fligerPCD = 1
+
+	for id in next, yo_spellsCD do
+		local starttime, duration, enabled = GetSpellCooldown(id)
+		
+		if starttime ~= 0 and duration > yo.fliger.pCDTimer then
+
+			--local timeleft = starttime + duration - now
+			local expirationTime = starttime + duration
+			local _, _, icon = GetSpellInfo( id)
+
+			if not self.pCD[fligerPCD] then self.pCD[fligerPCD] = CreateAuraIcon( self.pCD, fligerPCD, true)end				
+			UpdateAuraIcon( self.pCD[fligerPCD], "HELPFUL", icon, 0, nil, duration, expirationTime, id, fligerPCD)
+			fligerPCD = fligerPCD + 1
+		end
+	end	
+
+	for index = fligerPCD,	#self.pCD	do self.pCD[index]:Hide() end
+end
+
+
+local function MakeFligerFrame( self)
+	local pdebuffSize 	= yo.fliger.pDebuffSize
+	local tdebuffSize 	= yo.fliger.tDebuffSize
+	local pbuffSize 	= yo.fliger.pBuffSize
+	local cdSize		= yo.fliger.pCDSize
+	
+	_G["P_DEBUFF"]:SetSize( pdebuffSize,pdebuffSize)
+	_G["T_DEBUFF"]:SetSize(	tdebuffSize,tdebuffSize)
+	_G["P_BUFF"]:SetSize(	pbuffSize, 	pbuffSize)
+	_G["P_CD"]:SetSize(		cdSize,		cdSize)
+	--CreateAnchor("T_BUFF", 		"Target Buff", 	buffSize, 	buffSize,	400, 150, 	"CENTER", "CENTER")
+
+	if yo.fliger.tDebuffEnable then
+		local tDebuff = CreateFrame("Frame", nil, self)
+		tDebuff:SetPoint("CENTER", T_DEBUFF, "CENTER",  0, 0)
+		tDebuff:SetWidth( tdebuffSize)
+		tDebuff:SetHeight( tdebuffSize)
+		tDebuff.direction 	= yo.fliger.tDebuffDirect
+		tDebuff.unit 		= "target"
+		self.tDebuff 		= tDebuff
+	end
+
+	--local tBuff = CreateFrame("Frame", nil, self)
+	--tBuff:SetPoint("TOPLEFT", T_BUFF, "TOPLEFT",  0, 0)
+	--tBuff:SetWidth( buffSize * 10)
+	--tBuff:SetHeight( buffSize)
+	--tBuff.direction = "RIGHT"
+	--tBuff.unit 		= "target"
+	--tBuff.filter 	= "HELPFUL"
+	--self.tBuff 		= tBuff	
+
+	if yo.fliger.pBuffEnable then
+		local pBuff = CreateFrame("Frame", nil, self)
+		pBuff:SetPoint("CENTER", P_BUFF, "CENTER",  0, 0)
+		pBuff:SetWidth( pbuffSize)
+		pBuff:SetHeight( pbuffSize)
+		pBuff.direction = yo.fliger.pBuffDirect
+		pBuff.unit 		= "player"
+		self.pBuff 		= pBuff	
+	end
+
+	if yo.fliger.pDebuffEnable then
+		local pDebuff = CreateFrame("Frame", nil, self)
+		pDebuff:SetPoint("CENTER", P_DEBUFF, "CENTER",  0, 0)
+		pDebuff:SetWidth( pdebuffSize)
+		pDebuff:SetHeight( pdebuffSize)
+		pDebuff.direction 	= yo.fliger.pBuffDirect
+		pDebuff.unit 		= "player"
+		self.pDebuff 		= pDebuff
+	end
+
+	if yo.fliger.pCDEnable then
+		local pCD = CreateFrame("Frame", nil, self)
+		pCD:SetPoint("CENTER", P_CD, "CENTER",  0, 0)
+		pCD:SetWidth( cdSize)
+		pCD:SetHeight( cdSize)
+		pCD.direction 	= yo.fliger.pCDDirect
+		pCD.unit 		= "player"
+		self.pCD 		= pCD
+		UpdateSpels()
+	end
+end
+
+local function OnEvent( self, event, ...)
+	if event == "PLAYER_ENTERING_WORLD" then
+		self:UnregisterEvent("PLAYER_ENTERING_WORLD")
+
+		if not yo.fliger.enable then return end
+		self:RegisterEvent("PLAYER_TARGET_CHANGED")
+		self:RegisterUnitEvent("UNIT_AURA", "player", "target")
+
+		if yo.fliger.pCDEnable then
+			self:RegisterEvent("SPELL_UPDATE_COOLDOWN")
+		end				
+		
+		MakeFligerFrame( self)
+
+	elseif event == "UNIT_AURA" then
+		UpdateAura( self, ...)
+	elseif event == "PLAYER_TARGET_CHANGED" then
+		UpdateAura( self, "target")
+	elseif event == "SPELL_UPDATE_COOLDOWN" then
+		UpdateCD( self)
+	elseif event == "LEARNED_SPELL_IN_TAB" then
+		if yo.fliger.pCDEnable then
+			UpdateSpels()
+		end
+	end
+end
+
+local Fliger = CreateFrame("Frame", "yo_Fliger", UIParent)
+Fliger:RegisterEvent("PLAYER_ENTERING_WORLD")
+Fliger:RegisterEvent("LEARNED_SPELL_IN_TAB")
+Fliger:SetScript("OnEvent", OnEvent)
+
+--CreateAnchor("SPECIAL_P_BUFF_ICON_Anchor", "SPECIAL_P_BUFF",	C["filger"].buffs_size, C["filger"].buffs_size,			-300, -50, "CENTER", "CENTER")
--- Filters/RaidAuras.lua
+++ Filters/RaidAuras.lua
@@ -38,8 +38,176 @@ blackSpells = {
 	[264689] = true, -- Усталость от БЛа
 }
 
-whiteList = {
-	-- Battle for Azeroth
+RaidDebuffList = {
+	-- Mythic Dungeons
+	[209858] = true, -- Necrotic
+	[226512] = true, -- Sanguine
+	[240559] = true, -- Grievous
+	[240443] = true, -- Bursting
+	[196376] = true, -- Grievous Tear
+	--BFA Dungeons
+	--Freehold
+	[258323] = true, -- Infected Wound
+	[257775] = true, -- Plague Step
+	[257908] = true, -- Oiled Blade
+	[257436] = true, -- Poisoning Strike
+	[274389] = true, -- Rat Traps
+	[274555] = true, -- Scabrous Bites
+	[258875] = true, -- Blackout Barrel
+	[256363] = true, -- Ripper Punch
+	--Shrine of the Storm
+	[264560] = true, -- Choking Brine
+	[268233] = true, -- Electrifying Shock
+	[268322] = true, -- Touch of the Drowned
+	[268896] = true, -- Mind Rend
+	[268104] = true, -- Explosive Void
+	[267034] = true, -- Whispers of Power
+	[276268] = true, -- Heaving Blow
+	[264166] = true, -- Undertow
+	[264526] = true, -- Grasp of the Depths
+	[274633] = true, -- Sundering Blow
+	[268214] = true, -- Carving Flesh
+	[267818] = true, -- Slicing Blast
+	[268309] = true, -- Unending Darkness
+	[268317] = true, -- Rip Mind
+	[268391] = true, -- Mental Assault
+	[274720] = true, -- Abyssal Strike
+	--Siege of Boralus
+	[257168] = true, -- Cursed Slash
+	[272588] = true, -- Rotting Wounds
+	[272571] = true, -- Choking Waters
+	[274991] = true, -- Putrid Waters
+	[275835] = true, -- Stinging Venom Coating
+	[273930] = true, -- Hindering Cut
+	[257292] = true, -- Heavy Slash
+	[261428] = true, -- Hangman's Noose
+	[256897] = true, -- Clamping Jaws
+	[272874] = true, -- Trample
+	[273470] = true, -- Gut Shot
+	[272834] = true, -- Viscous Slobber
+	[257169] = true, -- Terrifying Roar
+	[272713] = true, -- Crushing Slam
+	-- Tol Dagor
+	[258128] = true, -- Debilitating Shout
+	[265889] = true, -- Torch Strike
+	[257791] = true, -- Howling Fear
+	[258864] = true, -- Suppression Fire
+	[257028] = true, -- Fuselighter
+	[258917] = true, -- Righteous Flames
+	[257777] = true, -- Crippling Shiv
+	[258079] = true, -- Massive Chomp
+	[258058] = true, -- Squeeze
+	[260016] = true, -- Itchy Bite
+	[257119] = true, -- Sand Trap
+	[260067] = true, -- Vicious Mauling
+	[258313] = true, -- Handcuff
+	[259711] = true, -- Lockdown
+	[256198] = true, -- Azerite Rounds: Incendiary
+	[256101] = true, -- Explosive Burst
+	[256044] = true, -- Deadeye
+	[256474] = true, -- Heartstopper Venom
+	--Waycrest Manor
+	[260703] = true, -- Unstable Runic Mark
+	[263905] = true, -- Marking Cleave
+	[265880] = true, -- Dread Mark
+	[265882] = true, -- Lingering Dread
+	[264105] = true, -- Runic Mark
+	[264050] = true, -- Infected Thorn
+	[261440] = true, -- Virulent Pathogen
+	[263891] = true, -- Grasping Thorns
+	[264378] = true, -- Fragment Soul
+	[266035] = true, -- Bone Splinter
+	[266036] = true, -- Drain Essence
+	[260907] = true, -- Soul Manipulation
+	[260741] = true, -- Jagged Nettles
+	[264556] = true, -- Tearing Strike
+	[265760] = true, -- Thorned Barrage
+	[260551] = true, -- Soul Thorns
+	[263943] = true, -- Etch
+	[265881] = true, -- Decaying Touch
+	[261438] = true, -- Wasting Strike
+	[268202] = true, -- Death Lens
+	-- Atal'Dazar
+	[252781] = true, -- Unstable Hex
+	[250096] = true, -- Wracking Pain
+	[250371] = true, -- Lingering Nausea
+	[253562] = true, -- Wildfire
+	[255582] = true, -- Molten Gold
+	[255041] = true, -- Terrifying Screech
+	[255371] = true, -- Terrifying Visage
+	[252687] = true, -- Venomfang Strike
+	[254959] = true, -- Soulburn
+	[255814] = true, -- Rending Maul
+	[255421] = true, -- Devour
+	[255434] = true, -- Serrated Teeth
+	[256577] = true, -- Soulfeast
+	--King's Rest
+	[270492] = true, -- Hex
+	[267763] = true, -- Wretched Discharge
+	[276031] = true, -- Pit of Despair
+	[265773] = true, -- Spit Gold
+	[270920] = true, -- Seduction
+	[270865] = true, -- Hidden Blade
+	[271564] = true, -- Embalming Fluid
+	[270507] = true, -- Poison Barrage
+	[267273] = true, -- Poison Nova
+	[270003] = true, -- Suppression Slam
+	[270084] = true, -- Axe Barrage
+	[267618] = true, -- Drain Fluids
+	[267626] = true, -- Dessication
+	[270487] = true, -- Severing Blade
+	[266238] = true, -- Shattered Defenses
+	[266231] = true, -- Severing Axe
+	[266191] = true, -- Whirling Axes
+	[272388] = true, -- Shadow Barrage
+	[271640] = true, -- Dark Revelation
+	[268796] = true, -- Impaling Spear
+	--Motherlode
+	[263074] = true, -- Festering Bite
+	[280605] = true, -- Brain Freeze
+	[257337] = true, -- Shocking Claw
+	[270882] = true, -- Blazing Azerite
+	[268797] = true, -- Transmute: Enemy to Goo
+	[259856] = true, -- Chemical Burn
+	[269302] = true, -- Toxic Blades
+	[280604] = true, -- Iced Spritzer
+	[257371] = true, -- Tear Gas
+	[257544] = true, -- Jagged Cut
+	[268846] = true, -- Echo Blade
+	[262794] = true, -- Energy Lash
+	[262513] = true, -- Azerite Heartseeker
+	[260829] = true, -- Homing Missle (travelling)
+	[260838] = true, -- Homing Missle (exploded)
+	[263637] = true, -- Clothesline
+	--Temple of Sethraliss
+	[269686] = true, -- Plague
+	[268013] = true, -- Flame Shock
+	[268008] = true, -- Snake Charm
+	[273563] = true, -- Neurotoxin
+	[272657] = true, -- Noxious Breath
+	[267027] = true, -- Cytotoxin
+	[272699] = true, -- Venomous Spit
+	[263371] = true, -- Conduction
+	[272655] = true, -- Scouring Sand
+	[263914] = true, -- Blinding Sand
+	[263958] = true, -- A Knot of Snakes
+	[266923] = true, -- Galvanize
+	[268007] = true, -- Heart Attack
+	--Underrot
+	[265468] = true, -- Withering Curse
+	[278961] = true, -- Decaying Mind
+	[259714] = true, -- Decaying Spores
+	[272180] = true, -- Death Bolt
+	[272609] = true, -- Maddening Gaze
+	[269301] = true, -- Putrid Blood
+	[265533] = true, -- Blood Maw
+	[265019] = true, -- Savage Cleave
+	[265377] = true, -- Hooked Snare
+	[265625] = true, -- Dark Omen
+	[260685] = true, -- Taint of G'huun
+	[266107] = true, -- Thirst for Blood
+	[260455] = true, -- Serrated Fangs
+		
 	-- Uldir
 	-- MOTHER
 	[268277] = true, -- Purifying Flame
@@ -64,27 +232,27 @@ whiteList = {
 	[272536] = true, -- Imminent Ruin
 	[274693] = true, -- Essence Shear
 	[272407] = true, -- Oblivion Sphere
-		-- Fetid Devourer
+	-- Fetid Devourer
 	[262313] = true, -- Malodorous Miasma
 	[262292] = true, -- Rotting Regurgitation
 	[262314] = true, -- Deadly Disease
-		-- Taloc
+	-- Taloc
 	[270290] = true, -- Blood Storm
 	[275270] = true, -- Fixate
 	[271224] = true, -- Plasma Discharge
 	[271225] = true, -- Plasma Discharge
-		-- Zul
+	-- Zul
 	[273365] = true, -- Dark Revelation
 	[273434] = true, -- Pit of Despair
 	[274195] = true, -- Corrupted Blood
 	[272018] = true, -- Absorbed in Darkness
-		-- Zek'voz, Herald of N'zoth
+	-- Zek'voz, Herald of N'zoth
 	[265237] = true, -- Shatter
 	[265264] = true, -- Void Lash
 	[265360] = true, -- Roiling Deceit
 	[265662] = true, -- Corruptor's Pact
 	[265646] = true, -- Will of the Corruptor
-		-- G'huun
+	-- G'huun
 	[263436] = true, -- Imperfect Physiology
 	[263227] = true, -- Putrid Blood
 	[263372] = true, -- Power Matrix
@@ -92,175 +260,5 @@ whiteList = {
 	[267409] = true, -- Dark Bargain
 	[267430] = true, -- Torment
 	[263235] = true, -- Blood Feast
-	[270287] = true, -- Blighted Ground 
-	
-	-- Mythic Dungeons
-	[226303] = true, -- Piercing Shards (Neltharion's Lair)
-	[227742] = true, -- Garrote (Karazhan)
-	[209858] = true, -- Necrotic
-	[226512] = true, -- Sanguine
-	[240559] = true, -- Grievous
-	[240443] = true, -- Bursting
-	[196376] = true, -- Grievous Tear
-	[200227] = true, -- Tangled Web
-
-	-- Legion
-	-- Antorus, the Burning Throne
-	-- Garothi Worldbreaker
-	[244590] = true, -- Molten Hot Fel
-	[244761] = true, -- Annihilation
-	[246920] = true, -- Haywire Decimation
-	[246369] = true, -- Searing Barrage
-	[246848] = true, -- Luring Destruction
-	[246220] = true, -- Fel Bombardment
-	[247159] = true, -- Luring Destruction
-	[244122] = true, -- Carnage
-	[244410] = true, -- Decimation
-	[245294] = true, -- Empowered Decimation
-	[246368] = true, -- Searing Barrage
-	-- Felhounds of Sargeras
-	[245022] = true, -- Burning Remnant
-	[251445] = true, -- Smouldering
-	[251448] = true, -- Burning Maw
-	[244086] = true, -- Molten Touch
-	[244091] = true, -- Singed
-	[244768] = true, -- Desolate Gaze
-	[244767] = true, -- Desolate Path
-	[244471] = true, -- Enflame Corruption
-	[248815] = true, -- Enflamed
-	[244517] = true, -- Lingering Flames
-	[245098] = true, -- Decay
-	[251447] = true, -- Corrupting Maw
-	[244131] = true, -- Consuming Sphere
-	[245024] = true, -- Consumed
-	[244071] = true, -- Weight of Darkness
-	[244578] = true, -- Siphon Corruption
-	[248819] = true, -- Siphoned
-	[254429] = true, -- Weight of Darkness
-	[244072] = true, -- Molten Touch
-	-- Antoran High Command
-	[245121] = true, -- Entropic Blast
-	[244748] = true, -- Shocked
-	[244824] = true, -- Warp Field
-	[244892] = true, -- Exploit Weakness
-	[244172] = true, -- Psychic Assault
-	[244388] = true, -- Psychic Scarring
-	[244420] = true, -- Chaos Pulse
-	[254771] = true, -- Disruption Field
-	[257974] = true, -- Chaos Pulse
-	[244910] = true, -- Felshield
-	[244737] = true, -- Shock Grenade
-		-- Portal Keeper Hasabel
-	[244016] = true, -- Reality Tear
-	[245157] = true, -- Everburning Light
-	[245075] = true, -- Hungering Gloom
-	[245240] = true, -- Oppressive Gloom
-	[244709] = true, -- Fiery Detonation
-	[246208] = true, -- Acidic Web
-	[246075] = true, -- Catastrophic Implosion
-	[244826] = true, -- Fel Miasma
-	[246316] = true, -- Poison Essence
-	[244849] = true, -- Caustic Slime
-	[245118] = true, -- Cloying Shadows
-	[245050] = true, -- Delusions
-	[245040] = true, -- Corrupt
-	[244607] = true, -- Flames of Xoroth
-	[244915] = true, -- Leech Essence
-	[244926] = true, -- Felsilk Wrap
-	[244949] = true, -- Felsilk Wrap
-	[244613] = true, -- Everburning Flames
-		-- Eonar the Life-Binder
-	[248326] = true, -- Rain of Fel
-	[248861] = true, -- Spear of Doom
-	[249016] = true, -- Feedback - Targeted
-	[249015] = true, -- Feedback - Burning Embers
-	[249014] = true, -- Feedback - Foul Steps
-	[249017] = true, -- Feedback - Arcane Singularity
-	[250693] = true, -- Arcane Buildup
-	[250691] = true, -- Burning Embers
-	[248795] = true, -- Fel Wake
-	[248332] = true, -- Rain of Fel
-	[250140] = true, -- Foul Steps
-		-- Imonar the Soulhunter
-	[248424] = true, -- Gathering Power
-	[247552] = true, -- Sleep Canister
-	[247565] = true, -- Slumber Gas
-	[250224] = true, -- Shocked
-	[248252] = true, -- Infernal Rockets
-	[247687] = true, -- Sever
-	[247716] = true, -- Charged Blasts
-	[247367] = true, -- Shock Lance
-	[250255] = true, -- Empowered Shock Lance
-	[247641] = true, -- Stasis Trap
-	[255029] = true, -- Sleep Canister
-	[248321] = true, -- Conflagration
-	[247932] = true, -- Shrapnel Blast
-	[248070] = true, -- Empowered Shrapnel Blast
-	[254183] = true, -- Seared Skin
-		-- Kin'garoth
-	[244312] = true, -- Forging Strike
-	[254919] = true, -- Forging Strike
-	[246840] = true, -- Ruiner
-	[248061] = true, -- Purging Protocol
-	[249686] = true, -- Reverberating Decimation
-	[246706] = true, -- Demolish
-	[246698] = true, -- Demolish
-	[245919] = true, -- Meteor Swarm
-	[245770] = true, -- Decimation
-	
-	[257978] = true, -- Decimation
-		-- Varimathras
-	[244042] = true, -- Marked Prey
-	[243961] = true, -- Misery
-	[248732] = true, -- Echoes of Doom
-	[243973] = true, -- Torment of Shadows
-	[244005] = true, -- Dark Fissure
-	[244093] = true, -- Necrotic Embrace
-	[244094] = true, -- Necrotic Embrace
-		-- The Coven of Shivarra
-	[244899] = true, -- Fiery Strike
-	[245518] = true, -- Flashfreeze
-	[245586] = true, -- Chilled Blood
-	[246763] = true, -- Fury of Golganneth
-	[245674] = true, -- Flames of Khaz'goroth
-	[245671] = true, -- Flames of Khaz'goroth
-	[245910] = true, -- Spectral Army of Norgannon
-	[253520] = true, -- Fulminating Pulse
-	[245634] = true, -- Whirling Saber
-	[253020] = true, -- Storm of Darkness
-	[245921] = true, -- Spectral Army of Norgannon
-	[250757] = true, -- Cosmic Glare
-		-- Aggramar
-	[244291] = true, -- Foe Breaker
-	[255060] = true, -- Empowered Foe Breaker
-	[245995] = true, -- Scorching Blaze
-	[246014] = true, -- Searing Tempest
-	[244912] = true, -- Blazing Eruption
-	[247135] = true, -- Scorched Earth
-	[247091] = true, -- Catalyzed
-	[245631] = true, -- Unchecked Flame
-	[245916] = true, -- Molten Remnants
-	[245990] = true, -- Taeshalach's Reach
-	[254452] = true, -- Ravenous Blaze
-	[244736] = true, -- Wake of Flame
-	[247079] = true, -- Empowered Flame Rend
-		-- Argus the Unmaker
-	[251815] = true, -- Edge of Obliteration
-	[248499] = true, -- Sweeping Scythe
-	[250669] = true, -- Soulburst
-	[251570] = true, -- Soulbomb
-	[248396] = true, -- Soulblight
-	[258039] = true, -- Deadly Scythe
-	[252729] = true, -- Cosmic Ray
-	[256899] = true, -- Soul Detonation
-	[252634] = true, -- Cosmic Smash
-	[252616] = true, -- Cosmic Beacon
-	[255200] = true, -- Aggramar's Boon
-	[255199] = true, -- Avatar of Aggramar
-	[258647] = true, -- Gift of the Sea
-	[253901] = true, -- Strength of the Sea
-	[257299] = true, -- Ember of Rage
-	[248167] = true, -- Death Fog
-	[258646] = true, -- Gift of the Sky
-	[253903] = true, -- Strength of the Sky
+	[270287] = true, -- Blighted Ground
 }
