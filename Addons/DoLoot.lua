cbe4faaf5d6d2411d31503bb30781a4aa9d32621
noisex <34417751+noisex@users.noreply.github.com>
2020-09-22 13:14:55 +0300

    doLoot

--- Addons/DoLoot.lua
+++ Addons/DoLoot.lua
@@ -26,7 +26,6 @@ local function afterClearIcons( index)
 		frame[i]:Hide()
 		frame[i].icon:SetTexture(nil)
 		frame[i].nameItem:SetText("")
-		--frame[i].nameDung:SetText("")
 		frame[i].stats:SetText("")
 		frame[i].onuse:SetText("")
 		frame[i].onEquip :SetText("")
@@ -82,12 +81,6 @@ local function CreateIcon( index)
 	nameItem:SetPoint("TOPLEFT", button, "TOPLEFT", 50, 0)
 	nameItem:SetPoint("TOPRIGHT", button, "TOPRIGHT", 10, 0)
 
-	--local nameDung = button:CreateFontString( nil, "OVERLAY")
-	--nameDung:SetFont( font, 13, "THINOUTLINE")
-	--nameDung:SetJustifyH("LEFT")
-	--nameDung:SetPoint("TOPLEFT", nameItem, "BOTTOMLEFT", 0, -0)
-	--nameDung:SetPoint("TOPRIGHT", nameItem, "BOTTOMRIGHT", 0, -0)
-
 	local stats = button:CreateFontString( nil, "OVERLAY")
 	stats:SetFont( font, 11)--, "THINOUTLINE")
 	stats:SetJustifyH("LEFT")
@@ -120,7 +113,6 @@ local function CreateIcon( index)
 	CreateStyle( bg, 2, 0, 0.3)
 
 	button.nameItem = nameItem
-	--button.nameDung = nameDung
 	button.icon 	= icon
 	button.stats 	= stats
 	button.onuse 	= onuse
@@ -144,11 +136,12 @@ local function checkDungeLoot( filterType)
 	local class, _, classID = UnitClass('player')
 	local frame = yo_DuLoot
 	frame.filterType = filterType
-	frame.headerKeeper.textHeader:SetText( class .. " / " .. frame.specLootName )
+	frame.headerKeeper.textHeader:SetText( myColorStr .. class .. " | " .. frame.specLootName )
 
 	EJ_SetLootFilter(classID, frame.specLootID )
 	EJ_SetDifficulty( 23)
 	--EJ_SetDifficulty( 23) -- 1, 2, 23 ( 8	Mythic Keystone	party	isHeroic, isChallengeMode)
+	--statTable = GetItemStatDelta("item1Link", "item2Link" [, returnTable])
 	local indexLo = 1
 
 	while instanceID do
@@ -164,75 +157,141 @@ local function checkDungeLoot( filterType)
 				for lootIndex = 1, EJ_GetNumLoot() do
 					local temptext = ""
 					local itemInfo = C_EncounterJournal.GetLootInfoByIndex( lootIndex)
-					local loolButton = CreateIcon( indexLo, itemInfo.link, itemInfo.icon)
-					loolButton.icon:SetTexture( itemInfo.icon)
-					loolButton.nameItem:SetText( link .. " |r" .. itemInfo.link)
-					--loolButton.nameDung:SetText( "")
-					loolButton.bg.link = itemInfo.link
-					loolButton.onuse:SetText("")
-					loolButton.onEquip:SetText( "")
-					loolButton.equipSet:SetText( "")
-					loolButton.iconBack:Show()
-					loolButton.bg:Show()
 
-					--local item = Item:CreateFromItemLink( itemInfo.link)
-					--print( indexLo, link, itemInfo.link)
-
+					local found, foundH, foundM, foundC, foundV = false, false, false, false, false
 					local stat = GetItemStats( itemInfo.link)
 					for i, data in pairs(stat) do
-						if i == "RESISTANCE0_NAME" then
-						elseif i == "ITEM_MOD_STAMINA_SHORT" then
-							temptext = temptext .. "|cff999999+" .. floor( data) .. " " .. _G[i] .. "\n|r"
-
-						elseif i == "ITEM_MOD_INTELLECT_SHORT" or i == "ITEM_MOD_AGILITY_SHORT" or i == "ITEM_MOD_STRENGTH_SHOR	T" then
-							temptext = temptext .. "|cff999999+" .. floor( data) .. " " .. _G[i] .. "\n|r"
-
-						else
-							temptext = temptext .. "|cff00ff00+" .. floor( data) .. " " .. _G[i] .. "\n|r"
-							--statTable = GetItemStatDelta("item1Link", "item2Link" [, returnTable])
+						if 		i == "RESISTANCE0_NAME" 				then
+						elseif	i == "ITEM_MOD_HASTE_RATING_SHORT" 		then foundH = true 	temptext = temptext .. "|cff00ff00+" .. data .. " " .. _G[i] .. "\n|r"
+						elseif 	i == "ITEM_MOD_MASTERY_RATING_SHORT"  	then foundM = true 	temptext = temptext .. "|cff00ff00+" .. data .. " " .. _G[i] .. "\n|r"
+						elseif 	i == "ITEM_MOD_CRIT_RATING_SHORT"  		then foundC = true 	temptext = temptext .. "|cff00ff00+" .. data .. " " .. _G[i] .. "\n|r"
+						elseif 	i == "ITEM_MOD_VERSATILITY"  			then foundV = true 	temptext = temptext .. "|cff00ff00+" .. data .. " " .. _G[i] .. "\n|r"
+						elseif  i == "ITEM_MOD_STAMINA_SHORT" 			then  				temptext = temptext .. "|cff999999+" .. data .. " " .. _G[i] .. "\n|r"
+						elseif 	i == "ITEM_MOD_INTELLECT_SHORT" or
+								i == "ITEM_MOD_AGILITY_SHORT" or
+								i == "ITEM_MOD_STRENGTH_SHORT" then							temptext = temptext .. "|cff999999+" .. data .. " " .. _G[i] .. "\n|r"
+						else																temptext = temptext .. "|cff00ff00+" .. floor( data) .. " " .. _G[i] .. "\n|r"
 						end
 					end
-					loolButton.stats:SetText(temptext, 1, 1, 1, 0)
 
-					local name, spellID = GetItemSpell( itemInfo.link)
-					if name then
-						local name, _, icon = GetSpellInfo(spellID)
-						local cooldown = GetSpellBaseCooldown(spellID)
-						local desc = GetSpellDescription(spellID)
-					--	print( "|cff999999" .. ITEM_SPELL_TRIGGER_ONUSE .. " " .. desc, "Восстановление: " .. SecondsToClock(cooldown/1000)) -- ITEM_SPELL_TRIGGER_ONEQUIP = "Equip:"; ITEM_SPELL_TRIGGER_ONPROC = "Chance on hit:";
-						temptext = "|cff999999" .. ITEM_SPELL_TRIGGER_ONUSE .. " " .. desc .. " Восстановление: " .. SecondsToClock(cooldown/1000)
-						loolButton.onuse:SetText(temptext, 1, 1, 1, 0)
+					if frame.seting.allstat and (( foundH and frame.seting.haste == false) or ( foundM and frame.seting.master == false) or ( foundC and frame.seting.crit ==  false) or ( foundV and frame.seting.versa == false)) then
+						found = false
+					elseif ( foundH and frame.seting.haste) or ( foundM and frame.seting.master) or ( foundC and frame.seting.crit) or ( foundV and frame.seting.versa) then
+						found = true
 					end
 
-					local tt = CreateFrame("GameTooltip", "yoFrame_ItemTooltip", UIParent, "GameTooltipTemplate")
-					tt:SetOwner( UIParent, "ANCHOR_NONE")
-					tt:SetHyperlink( itemInfo.link)
-					tt:Show()
+					--print(found, itemInfo.link, frame.seting.haste, frame.seting.master, frame.seting.crit, frame.seting.versa, frame.seting.allstat, foundH, foundM, foundC, foundV )
+					if found or filterType == 13 then
+						local loolButton = CreateIcon( indexLo, itemInfo.link, itemInfo.icon)
+						loolButton.icon:SetTexture( itemInfo.icon)
+						loolButton.nameItem:SetText( link .. " |r" .. itemInfo.link)
+						loolButton.bg.link = itemInfo.link
+						loolButton.stats:SetText(temptext, 1, 1, 1, 0)
+						loolButton.onuse:SetText("")
+						loolButton.onEquip:SetText( "")
+						loolButton.equipSet:SetText( "")
+						loolButton.iconBack:Show()
+						loolButton.bg:Show()
+						--local item = Item:CreateFromItemLink( itemInfo.link)
+						--print( indexLo, link, itemInfo.link)
 
-					for x = 5, tt:NumLines() do
-						local line = _G['yoFrame_ItemTooltipTextLeft'..x]
-						local lineText = line:GetText()
-						if lineText then
-							local lr, lg, lb = line:GetTextColor()
-							if lineText:match( ITEM_SPELL_TRIGGER_ONEQUIP) then
-								loolButton.onEquip:SetText( format( " %s%s ", hex( lr, lg, lb), lineText))
+						local name, spellID = GetItemSpell( itemInfo.link)
+						if name then
+							local name, _, icon = GetSpellInfo(spellID)
+							local cooldown = GetSpellBaseCooldown(spellID)
+							local desc = GetSpellDescription(spellID)
+							temptext = "|cff999999" .. ITEM_SPELL_TRIGGER_ONUSE .. " " .. desc .. " Восстановление: " .. SecondsToClock(cooldown/1000)
+							loolButton.onuse:SetText(temptext, 1, 1, 1, 0)
+						end
 
-							elseif lineText:match( "Комплект ") then
-								local itemSetID = select( 16, GetItemInfo( itemInfo.link))
-								loolButton.equipSet:SetText( "|cffffff00" .. GetItemSetInfo( itemSetID) .. "\n|cff999999" .. lineText)
+						local tt = CreateFrame("GameTooltip", "yoFrame_ItemTooltip", UIParent, "GameTooltipTemplate")
+						tt:SetOwner( UIParent, "ANCHOR_NONE")
+						tt:SetHyperlink( itemInfo.link)
+						tt:Show()
+
+						for x = 5, tt:NumLines() do
+							local line = _G['yoFrame_ItemTooltipTextLeft'..x]
+							local lineText = line:GetText()
+							if lineText then
+								local lr, lg, lb = line:GetTextColor()
+								if lineText:match( ITEM_SPELL_TRIGGER_ONEQUIP) then
+									loolButton.onEquip:SetText( format( " %s%s ", hex( lr, lg, lb), lineText))
+
+								elseif lineText:match( "Комплект ") then
+									local itemSetID = select( 16, GetItemInfo( itemInfo.link))
+									loolButton.equipSet:SetText( "|cffffff00" .. GetItemSetInfo( itemSetID) .. "\n|cff999999" .. lineText)
+								end
 							end
 						end
-					end
-					tt:Hide()
+						tt:Hide()
 
-					loolButton:Show()
-					indexLo = indexLo + 1
+						loolButton:Show()
+						indexLo = indexLo + 1
+					end
 				end
 			end
 			yo_DuLoot:Show()
 		end
 	end
 	afterClearIcons( indexLo)
+end
+
+local function settingDoIt( self)
+	if not self.setting then
+		local setting = CreateFrame("Frame", nil, self)
+		setting:SetPoint("TOPLEFT", self, "TOPRIGHT", 60, 0)
+		setting:SetSize(170, 140)
+		setting:Hide()
+		CreateStyle( setting, 2)
+
+		local close = CreateFrame("Button", nil, setting, "UIPanelCloseButton")
+		close:SetPoint("TOPRIGHT", setting, "TOPRIGHT", 8, 7)
+
+		local hasteCheck = CreateFrame("CheckButton", nil, setting, "InterfaceOptionsCheckButtonTemplate")
+		hasteCheck:SetSize( 20, 20)
+		hasteCheck:SetPoint("TOPLEFT", setting, "TOPLEFT", 10, -10)
+		hasteCheck:SetChecked( self.haste)
+		hasteCheck.Text:SetText( "Скорость")
+		hasteCheck:SetScript("OnClick", function() self.haste = hasteCheck:GetChecked() checkDungeLoot( self:GetParent().filterType) end)
+
+		local critCheck = CreateFrame("CheckButton", nil, setting, "InterfaceOptionsCheckButtonTemplate")
+		critCheck:SetSize( 20, 20)
+		critCheck:SetPoint("TOPLEFT", hasteCheck, "TOPLEFT", 0, -25)
+		critCheck:SetChecked( self.crit)
+		critCheck.Text:SetText( "Критический")
+		critCheck:SetScript("OnClick", function() self.crit = critCheck:GetChecked() checkDungeLoot( self:GetParent().filterType) end)
+
+		local masterCheck = CreateFrame("CheckButton", nil, setting, "InterfaceOptionsCheckButtonTemplate")
+		masterCheck:SetSize( 20, 20)
+		masterCheck:SetPoint("TOPLEFT", critCheck, "TOPLEFT", 0, -25)
+		masterCheck:SetChecked( self.master)
+		masterCheck.Text:SetText( "Искусность")
+		masterCheck:SetScript("OnClick", function() self.master = masterCheck:GetChecked() checkDungeLoot( self:GetParent().filterType) end)
+
+		local versaCheck = CreateFrame("CheckButton", nil, setting, "InterfaceOptionsCheckButtonTemplate")
+		versaCheck:SetSize( 20, 20)
+		versaCheck:SetPoint("TOPLEFT", masterCheck, "TOPLEFT", 0, -25)
+		versaCheck:SetChecked( self.versa)
+		versaCheck.Text:SetText( "Универсальность")
+		versaCheck:SetScript("OnClick", function() self.versa = versaCheck:GetChecked() checkDungeLoot( self:GetParent().filterType) end)
+
+		local allCheck = CreateFrame("CheckButton", nil, setting, "InterfaceOptionsCheckButtonTemplate")
+		allCheck:SetSize( 20, 20)
+		allCheck:SetPoint("TOPLEFT", versaCheck, "TOPLEFT", 0, -25)
+		allCheck:SetChecked( self.allstat)
+		allCheck.Text:SetText( "Статы на одной вещи")
+		allCheck.Text:SetTextColor(1, 1, 0.5)
+		allCheck:SetScript("OnClick", function() self.allstat = allCheck:GetChecked() checkDungeLoot( self:GetParent().filterType) end)
+
+		setting.allCheck 	= allCheck
+		setting.masterCheck = masterCheck
+		setting.versaCheck 	= versaCheck
+		setting.critCheck 	= critCheck
+		setting.hasteCheck 	= hasteCheck
+		setting.close = close
+		self.setting = setting
+	end
+	self.setting:SetShown( not self.setting:IsShown())
 end
 
 local function CreateDuLoot( self)
@@ -256,7 +315,6 @@ local function CreateDuLoot( self)
 	local textHeader = headerKeeper:CreateFontString(nil, "OVERLAY")
 	textHeader:SetFont( font, 13, "THINOUTLINE")
 	textHeader:SetText("")
-	textHeader:SetTextColor(1, 1, 0, 1)
 	textHeader:SetPoint("TOP", headerKeeper, "TOP", 0, -2)
 
 	local grabber = CreateFrame("Button", nil, headerKeeper)
@@ -276,6 +334,16 @@ local function CreateDuLoot( self)
 	headerKeeper.grabber 	= grabber
 	headerKeeper.textHeader = textHeader
 
+	local seting = CreateFrame("Button", nil, self)
+	seting:SetPoint("RIGHT", close, "LEFT", 0, 0)
+	seting:SetSize( 15, 15)
+	seting:SetNormalTexture("Interface\\GossipFrame\\HealerGossipIcon")
+	seting:SetHighlightTexture("Interface\\GossipFrame\\BinderGossipIcon")
+	seting:SetScript("OnEnter", function(self, ...) self.shadow:SetBackdropBorderColor( myColor.r, myColor.g, myColor.b, 0.9)	end)
+	seting:SetScript("OnLeave", function(self, ...) self.shadow:SetBackdropBorderColor( 0.09, 0.09, 0.09, 0.9)	end)
+	seting:SetScript("OnClick", settingDoIt)
+	CreateStyleSmall( seting, 2)
+
 	scrollFrame = CreateFrame("ScrollFrame", nil, self, "UIPanelScrollFrameTemplate")
 	scrollFrame:SetPoint("TOPLEFT", self, "TOPLEFT", 0, -25)
 	scrollFrame:SetPoint("BOTTOMRIGHT", self, "BOTTOMRIGHT", -3, 3)
@@ -293,9 +361,16 @@ local function CreateDuLoot( self)
 	scrollFrame:SetUserPlaced(true)
 	scrollFrame:SetScript("OnSizeChanged", function(self) scrollChild:SetWidth(self:GetWidth()) end)
 
+	self.seting 		= seting
 	self.headerKeeper	= headerKeeper
 	self.scrollFrame 	= scrollFrame
 	self.scrollChild 	= scrollChild
+
+	self.seting.haste 	= true
+	self.seting.crit 	= true
+	self.seting.versa 	= true
+	self.seting.master 	= true
+	self.seting.allstat	= false
 end
 
 local function OnEvent( self, event, ...)
